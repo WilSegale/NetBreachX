697edbceb7af4d65390d3c1f71d5a1813281750f
Will Segale <39059459+WilSegale@users.noreply.github.com>
2025-03-09 13:57:29 -0400

    update

--- linux/NetBreachX.py
+++ linux/NetBreachX.py
@@ -6,9 +6,9 @@ def show_help():
 
     HelpFile = open("HELP.txt", "w")
 
-    terminalCommand("cat Bash_Help_message.txt")    
     # Open a file and print its content
     file_path = 'HelpLogo.txt'
+    terminalCommand("cat HelpLogo.txt")    
 
     with open(file_path, 'r') as file:
         content = file.read()
